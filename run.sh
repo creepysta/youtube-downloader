9d2ba3c3718197ad5d65d348265449b7ca5560fc
creepysta <sweetnanispregnantghost@yahoo.com>
2020-12-27 03:15:21 +0530

    fixed run scripts

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@ virtualenv env
 ./env/Scripts/activate
 pip install -r requirements
 pip install numpy==1.19.0
+cp formats.py env/Lib/site-packages/tube_dl/formats.py
+cp __main__.py env/Lib/site-packages/tube_dl/__main__.py
 python script.py
 
