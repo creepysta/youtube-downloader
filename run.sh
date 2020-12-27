5e143302510a6afd027f469d50594566e1bd3cca
creepysta <sweetnanispregnantghost@yahoo.com>
2020-12-27 19:37:23 +0530

    minor change

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ virtualenv env
 ./env/Scripts/activate
 pip install -r requirements
 pip install numpy==1.19.0
-formats.py "env/Lib/site-packages/tube_dl/formats.py"
-__main__.py "env/Lib/site-packages/tube_dl/__main__.py"
+cp formats.py "env/Lib/site-packages/tube_dl/formats.py"
+cp __main__.py "env/Lib/site-packages/tube_dl/__main__.py"
 python script.py
 
