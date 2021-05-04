1ef2ea6db4ef85e6f5242a4a4bb6877b8b944ff1
creepysta <sweetnanispregnantghost@yahoo.com>
2021-05-04 13:14:09 +0530

    open file mgr when done

--- run.bat
+++ run.bat
@@ -6,3 +6,4 @@ pip install numpy==1.19.0
 copy formats.py "env/Lib/site-packages/tube_dl/formats.py" /y
 copy __main__.py "env/Lib/site-packages/tube_dl/__main__.py" /y
 python script.py
+explorer audios
